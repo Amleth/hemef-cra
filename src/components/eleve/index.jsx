de0733def388dfd0ca3dcccf4ffaf1329df55b90
plarocca <pierre.la-rocca@etu.utc.fr>
2020-06-10 15:51:21 +0200

    eleve : TextField + correctifs dates

--- src/components/eleve/index.jsx
+++ src/components/eleve/index.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import {
   Box,
   Grid,
+  TextField,
   Container,
   Typography
 } from '@material-ui/core'
@@ -32,15 +33,16 @@ class eleve extends React.Component {
       return <div>Données en cours de téléchargement...</div>
     } else {
       //affichage conditionnel pour le champ date de sortie
+      let eleve = this.state.eleveData
       let intitule_date_sortie_conservatoire = null
       let champ_date_sortie_conservatoire = null
       if (this.state.eleveData.hypothese_cursus_date_sortie_conservatoire) {
         intitule_date_sortie_conservatoire = "Date de sortie supposée :"
-        champ_date_sortie_conservatoire = <Box p={2}>{this.state.eleveData.hypothese_cursus_date_sortie_conservatoire}</Box>
+        champ_date_sortie_conservatoire = this.state.eleveData.hypothese_cursus_date_sortie_conservatoire
       }
       else if (this.state.eleveData.cursus_date_sortie_conservatoire) {
         intitule_date_sortie_conservatoire = "Date de sortie :"
-        champ_date_sortie_conservatoire = <Box p={2}>{this.state.eleveData.cursus_date_sortie_conservatoire.split("^^")[0]}</Box>
+        champ_date_sortie_conservatoire = makeDate(this.state.eleveData.cursus_date_sortie_conservatoire)
       }
 
       //affichage conditionnel pour le champ motif de sortie
@@ -48,89 +50,66 @@ class eleve extends React.Component {
       let champ_motif_sortie_conservatoire = null
       if (this.state.eleveData.hypothese_cursus_motif_sortie) {
         intitule_motif_sortie_conservatoire = "Motif de sortie supposé :"
-        champ_motif_sortie_conservatoire = <Box p={2}>{this.state.eleveData.hypothese_cursus_motif_sortie}</Box>
+        champ_motif_sortie_conservatoire = this.state.eleveData.hypothese_cursus_motif_sortie
       }
       else if (this.state.eleveData.cursus_motif_sortie) {
         intitule_motif_sortie_conservatoire = "Motif de sortie :"
-        champ_motif_sortie_conservatoire = <Box p={2}>{this.state.eleveData.cursus_motif_sortie.split("^^")[0]}</Box>
+        champ_motif_sortie_conservatoire = this.state.eleveData.cursus_motif_sortie
       }
 
+
       //affichage conditionnel du pseudonyme
-      let pseudonyme = " "
-      if (this.state.eleveData.pseudonyme){
-        pseudonyme = this.state.eleveData.pseudonyme
+      let infos_eleve = null
+
+      const dateNaissance = makeDate(eleve.date_de_naissance)
+
+      if (this.state.eleveData.pseudonyme) {
+        infos_eleve =
+          <Grid container direction='row' justify='flex-start' alignItems='center'>
+            {makeTextField('Nom', eleve.nom)}
+            {makeTextField('Prénom', eleve.prenom)}
+            {makeTextField('Pseudonyme', eleve.pseudonyme)}
+            {makeTextField('Sexe', eleve.sexe)}
+            {makeTextField('Date de naissance', dateNaissance)}
+            {makeTextField('Lieu de naissance', eleve.nait_a_label)}
+          </Grid>
+      } else {
+        infos_eleve =
+          <Grid container direction='row' justify='flex-start' alignItems='center'>
+            {makeTextField('Nom', eleve.nom)}
+            {makeTextField('Prénom', eleve.prenom)}
+            {makeTextField('Sexe', eleve.sexe)}
+            {makeTextField('Date de naissance', dateNaissance)}
+            {makeTextField('Lieu de naissance', eleve.nait_a_label)}
+          </Grid>
       }
+
+      //Creation conditionnelle des infos_cursus
+      let infos_cursus = 
+        <Grid container direction='row' justify='flex-start' alignItems='center'>
+              {makeTextField('Cote AN Registre', eleve.cote_AN_registre)}
+              {makeTextField("Date d'entrée au concervatoire ", makeDate(eleve.cursus_date_entree_conservatoire))}
+              {makeTextField("Motif d'entrée", eleve.cursus_motif_admission)}
+              {makeTextField(intitule_date_sortie_conservatoire, champ_date_sortie_conservatoire)}
+              {makeTextField(intitule_motif_sortie_conservatoire, champ_motif_sortie_conservatoire)}
+            </Grid>
+
 
       return (
         <Container>
           <Typography component='h1' variant='h3'>Page de l'élève</Typography>
-          <Grid container direction='row' justify='flex-start' alignItems='center'>
-            <Grid item>
-              <Typography variant='button'>
-                <Box p={2}>Nom :</Box>
-                <Box p={2}>Prénom :</Box>
-                <Box p={2}>Pseudonyme :</Box>
-                <Box p={2}>Sexe :</Box>
-                <Box p={2}>Date de naissance :</Box>
-                <Box p={2}>Lieu de naissance :</Box>
-              </Typography>
-            </Grid>
-
-            <Grid item>
-              <Typography variant='body1'>
-                <Box p={2}>{this.state.eleveData.nom}</Box>
-                <Box p={2}>{this.state.eleveData.prenom}</Box>
-                <Box p={2}>{pseudonyme}</Box>
-                <Box p={2}>{this.state.eleveData.sexe}</Box>
-                <Box p={2}>{this.state.eleveData.date_de_naissance.split("^^")[0]}</Box>
-                <Box p={2}>{this.state.eleveData.nait_a_label}</Box>
-              </Typography>
-            </Grid>
-          </Grid>
-          {/* Insérer des intitulés optionnels/conditionnels pour les hypothèses et potentiellement vides */}
-          <Typography component='h2' variant='h4'>Informations relatives au cursus</Typography>
-          <Grid container direction='row' justify='flex-start' alignItems='center'>
-            <Grid item>
-              <Typography variant='button' >
-                <Box p={2}>Cote registre AN :</Box>
-                <Box p={2}>Date d'entrée au conservatoire :</Box>
-                <Box p={2}>Motif d'admission :</Box>
-              </Typography>
-            </Grid>
-
-            <Grid item>
-              <Typography variant='body1' >
-                <Box p={2}>{this.state.eleveData.cote_AN_registre}</Box>
-                <Box p={2}>{this.state.eleveData.cursus_date_entree_conservatoire.split("^^")[0]}</Box>
-                <Box p={2}>{this.state.eleveData.cursus_motif_admission}</Box>
-              </Typography>
-            </Grid>
-          </Grid>
-
-          <Grid container direction='row' justify='flex-start' alignItems='center'>
-            <Grid item>
-              <Typography variant='button' >
-                <Box p={2}>{intitule_date_sortie_conservatoire}</Box>
-                <Box p={2}>{intitule_motif_sortie_conservatoire}</Box>
-              </Typography>
-            </Grid>
-
-            <Grid item>
-              <Typography variant='body1'>
-                {champ_date_sortie_conservatoire}
-                {champ_motif_sortie_conservatoire}
-              </Typography>
-            </Grid>
-          </Grid>
+          {infos_eleve}
+          <Typography component='h2' variant='h4'>Informations de cursus</Typography>
+          {infos_cursus}
           <MaterialTable
             title="Classes suivies"
             columns={[
-              {title: "Discipline", field: "discipline_enseignée_label"},
-              {title: "Professeur", field: "professeur_label"},{
+              { title: "Discipline", field: "discipline_enseignée_label" },
+              { title: "Professeur", field: "professeur_label" }, {
                 title: "Date d'entrée", render: row => {
                   let date = row.date_entrée.split('T')[0]
                   date = date.split('-')
-                  return (date[2] + "-" + date[1] + "-" + date[0])
+                  return (date[2] + "/" + date[1] + "/" + date[0])
                 }
               }
             ]}
@@ -139,7 +118,7 @@ class eleve extends React.Component {
               const parcoursClasseID = selectedRow.classe_parcourue.slice(-36)
               this.props.history.push('/classe_cursus/' + parcoursClasseID)
             }}
-            
+
           />
         </Container>
 
@@ -148,4 +127,25 @@ class eleve extends React.Component {
   }
 }
 
+function makeTextField(f, v) {
+  return (
+    <TextField
+      label={f}
+      defaultValue={v}
+      InputProps={{
+        readOnly: true
+      }}
+    />
+  )
+}
+
+function makeDate(d) {
+  if (d){
+    let tempDate = d.split('^^')[0]
+    return (tempDate.split('-')[2] + '/' + tempDate.split('-')[1] + '/' + tempDate.split('-')[0])
+  }
+}
+
 export default withRouter(eleve)
+
+
