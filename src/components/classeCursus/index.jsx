15283912cb449742edbf3fa9437673fd34b290ac
plarocca <pierre.la-rocca@etu.utc.fr>
2020-06-11 15:05:27 +0200

    classeCursus Affichage + Table

--- src/components/classeCursus/index.jsx
+++ src/components/classeCursus/index.jsx
@@ -27,7 +27,7 @@ class classeCursus extends React.Component {
     }
     )
   }
-  
+
   render() {
     if (!this.state.classeCursusData) {
       return <div>Données en cours de téléchargement...</div>
@@ -39,76 +39,78 @@ class classeCursus extends React.Component {
       // let tableInterpretations = null
 
       //A ADAPTER AVEC LA MAJ API
-      if (this.state.classeCursusData.prix !== undefined) {;
+      if (this.state.classeCursusData.prix !== undefined) {
         tablePrix =
-          <Grid>
-            <Box m={3}>
               <MaterialTable
                 title='Prix Obtenus'
                 columns={[
                   { title: "Intitulé", field: "prix_nom" },
-                  { title: "Date", field: "string",
-                render : (r) => {
-                  if (r.prix_année){
-                    console.log(r.prix_année)
-                  }
-                  else if (r.prix_hypothèse_année){
-                    console.log(r.prix_hypothèse_année)
-                  }
-                } },
+                  {
+                    title: "Date", field: "string",
+                    render: (r) => {
+                      if (r.prix_année) {
+                       return(r.prix_année)
+                      }
+                      else if (r.prix_hypothèse_année) {
+                        return('[' + r.prix_hypothèse_année.split("^^")[0] + ']')
+                      }
+                    }
+                  },
 
                   { title: "Discipline", field: "prix_discipline_label" }
                 ]}
                 data={this.state.classeCursusData.prix}
-                // onRowClick={((evt, selectedRow) => {
-                //   const workId = selectedRow.work.slice(-36)
-                //   this.props.history.push('/work/' + workId)
-                // })}
               >
 
               </MaterialTable>
-            </Box>
-          </Grid>
       }
 
-      let informations_clés = null
+      let informations_clés =
+        <Grid container direction='column' justify='flex-start' >
+          {makeTextField('Élève', parcours_classe.prénom + ' ' + parcours_classe.nom)}
+          {makeTextField('Dicsipline', parcours_classe.discipline_label)}
+        </Grid>;
 
-      if (this.state.classeCursusData.hypothèse_date_sortie){
-        informations_clés =
-        <Grid container direction='row' justify='flex-start' alignItems='center'>
-            {makeTextField('Élève', parcours_classe.nom + parcours_classe.prénom)})}
-          </Grid>
+      let infos_parcours = null
+
+      if (this.state.classeCursusData.date_sortie) {
+        infos_parcours =
+          <Grid container direction='column' justify='flex-start' >
+            {makeTextField("Date d'entrée", makeDate(parcours_classe.date_entrée.split('T')[0]))}
+            {makeTextField("Motif d'entrée", parcours_classe.motif_entrée)}
+            {makeTextField("Date de sortie", makeDate(parcours_classe.date_sortie.split('T')[0]))}
+            {makeTextField("Motif de sortie", parcours_classe.motif_sortie)}
+          </Grid>;
+      }
+      else if (this.state.classeCursusData.hypothèse_date_sortie) {
+        infos_parcours =
+          <Grid container direction='column' justify='flex-start' >
+            {makeTextField("Date d'entrée", makeDate(parcours_classe.date_entrée.split('T')[0]))}
+            {makeTextField("Motif d'entrée", parcours_classe.motif_entrée)}
+            {makeTextField("Hypothèse date de sortie", parcours_classe.hypothèse_date_sortie)}
+            {makeTextField("Motif de sortie", parcours_classe.motif_sortie)}
+          </Grid>;
       }
 
+      const observation = parcours_classe.observations_élève
+      ? parcours_classe.observations_élève
+          .trim()
+          .split(';')
+          .map((_) => _.trim())
+          .filter((_) => _.length > 0)
+          .join(' • ')
+      : ''
       return (
         <Container>
           <Typography component='h1' variant='h3'>Parcours Classe</Typography>
-          <Grid container direction='row' justify='flex-start' alignItems='center'>
-            <Grid item>
-              <Typography variant='button' component='h2'>
-                <Box p={2}>Elève :</Box>
-                <Box p={2}>Discipline :</Box>
-                <Box p={2}>Professeur :</Box>
-                <Box p={2}>Date d'entrée :</Box>
-                <Box p={2}>Motif d'entrée :</Box>
-                <Box p={2}>Date sortie :</Box>
-                <Box p={2}>Motif de sortie :</Box>
-                <Box p={2}>Observations :</Box>
-              </Typography>
-            </Grid>
-
-            <Grid item>
-              <Typography variant='body1' >
-                {/* <Box p={2}>{this.state.classeCursusData[0].prénom}</Box> */}
-                {/* <Box p={2}>{this.state.musicianData.given_name}</Box>
-                <Box p={2}>{datesMusicien}</Box>
-                <Box p={2}>{this.state.musicianData.status_label}</Box>
-                <Box p={2}>{this.state.musicianData.nationality_label}</Box>
-                <Box p={2}>{this.state.musicianData.style_label}</Box>
-                <Box p={2}>{this.state.musicianData.description}</Box> */}
-              </Typography>
-            </Grid>
-          </Grid>
+          {informations_clés}
+          <br />
+          <br />
+          {infos_parcours}
+          <br />
+          Observations : 
+          <Box fontStyle='italic'>{observation && <Typography>{observation}</Typography>}</Box>
+          <br/>
           {tablePrix}
         </Container>
       )
@@ -129,7 +131,7 @@ function makeTextField(f, v) {
 }
 
 function makeDate(d) {
-  if (d){
+  if (d) {
     let tempDate = d.split('^^')[0]
     return (tempDate.split('-')[2] + '/' + tempDate.split('-')[1] + '/' + tempDate.split('-')[0])
   }
