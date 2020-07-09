286222141469ca3c8ca294d5374776681c7ac886
plarocca <pierre.la-rocca@etu.utc.fr>
2020-07-09 12:12:19 +0200

    affichagecustom  Tooltype Recharts

--- src/components/graph_sexe_discipline/index.jsx
+++ src/components/graph_sexe_discipline/index.jsx
@@ -26,6 +26,7 @@ class graph_sexe_discipline extends PureComponent {
                     tempObj.discipline = key
                     tempObj.Homme = value["hommes"]
                     tempObj.Femme = value["femmes"]
+                    tempObj.total = Number(value.hommes) + Number(value.femmes)
                     newdata.push(tempObj)
             }
             console.log(newdata)
@@ -34,8 +35,24 @@ class graph_sexe_discipline extends PureComponent {
         )
     }
 
+
     render() {
         const data = this.state.data
+
+        const CustomTooltip = ({ active, payload, label }) => {
+            if (active) {
+              return (
+                <div className="custom-tooltip">
+                <p className="label">{`${label} : ${payload[0].payload.total}`} inscrit.e.s</p>
+                <p className="hommes">Hommes :  {payload[0].payload.Homme} </p>
+                <p className="hommes">Femmes :  {payload[0].payload.Femme} </p>
+                </div>
+              );
+            }
+
+            return null;
+        }
+
         return (
             <Container>
                 <Typography component='h1' variant='h4'>Répartition sexuée des disciplines</Typography>
@@ -50,7 +67,7 @@ class graph_sexe_discipline extends PureComponent {
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="discipline" height={160} angle={-45} textAnchor="end" interval={0} />
                     <YAxis />
-                    <Tooltip />
+                    <Tooltip content={<CustomTooltip/>}/>
                     <Legend />
                     <Bar dataKey="Homme" stackId="a" fill="#34c3eb" />
                     <Bar dataKey="Femme" stackId="a" fill="#eba234" />
