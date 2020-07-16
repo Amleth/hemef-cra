10203eb929788541a26656262eec9af56f4b8369
plarocca <pierre.la-rocca@etu.utc.fr>
2020-07-16 10:50:32 +0200

    conversion Prix en composant fonction

--- src/components/prix/index.jsx
+++ src/components/prix/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MaterialTable from 'material-table'
 import { withRouter } from 'react-router'
-import axios from 'axios'
 import { CircularProgress, Container, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -16,27 +15,17 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 
-class prix extends React.Component {
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            prixData: [],
-            NomPrix:''
-        }
+function Prix({history}) {
 
-        this.handleNomPrixChange = this.handleNomPrixChange.bind(this);
-       
-    }
+    const classes = useStyles()
+    const [prixData, setData] = useState([])
+    const [nameP, setNomPrix] = useState('')
 
-    handleNomPrixChange(e){
-        e.preventDefault()
-        this.setState({NomPrix: e.target.value});
-    }
-
-    componentDidMount() {   
-        axios.get('http://data-iremus.huma-num.fr/api/hemef/prix').then(res => {
-            const initData = res.data
+    async function fetchData() {
+        const res = await fetch('http://data-iremus.huma-num.fr/api/hemef/prix')
+        res.json().then((res) => {
+            console.log(res)
+            const initData = res
             let dataArray = []
             for (const o of initData) {
                 if (!o.année) {
@@ -49,80 +38,81 @@ class prix extends React.Component {
                 }
                 dataArray.push(o)
             }
-            this.setState({ prixData: dataArray })
+            setData(res)
         })
     }
 
-    
-
-    render() {
-
-        let nomPrix = this.state.prixData.map((_) => _.nom_label).map((_) => (_ ? _.toLowerCase() : ''))
-        let o = {}
-        for (let s of nomPrix) o[s] = null
-        nomPrix = Object.keys(o)
-            .filter((s) => s.length > 0)
-            .sort()
-
-        if (!this.state.prixData) {
-            return (<Container maxWidth='md' align='center'>
-                <CircularProgress />
-            </Container>)
-        } else {
-            return (
-                <>
-                    <FormControl className={useStyles.formControl}>
-                        <InputLabel id='type-select-label'>Noms de Prix</InputLabel>
-                        <Select
-                            labelId='nom-select-label'
-                            id='nom-select'
-                            onChange={this.handleNomPrixChange}
-                            value={this.state.NomPrix}
-                        >
-                            <MenuItem value=''>
-                                <em>Pas de filtre</em>
-                            </MenuItem>
-                            {nomPrix.map((s) => (
-                                <MenuItem key={s} value={s}>
-                                    {s}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-
-                    <div style={{ maxWidth: '100%' }}>
-                        <MaterialTable
-                            title='Liste des prix recensés'
-                            columns={[
-                                { title: 'Nom du prix', field: 'nom_label', defaultFilter: this.state.NomPrix},
-                                { title: 'Discipline', field: 'discipline_label' },
-                                { title: "Année d'attribution", field: 'année' },
-                                { title: 'Prénom lauréat.e', field: 'élève_prénom' },
-                                { title: 'Nom lauréat.e', field: 'élève_nom' },
-                                { title: 'Cote AN du registre lauréat.e', field: 'élève_cote_AN_registre' },
-                                { title: 'Sexe lauréat.e', field: 'élève_sexe', sorting: false }
-                            ]}
-                            options={{
-                                pageSize: 50,
-                                pageSizeOptions: [20, 50, 100],
-                                filtering: true,
-                                sorting: true,
-                                cellStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' },
-                                headerStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' }
-                            }}
-                            data={this.state.prixData}
-                            onRowClick={((evt, selectedRow) => {
-                                const eleveId = selectedRow.élève.slice(-36)
-                                this.props.history.push('/eleve/' + eleveId)
-                            })}
-                        >
-                        </MaterialTable>
-                    </div>
-                </>
-            )
-        }
+    const handleNomPrixChange = (event) => {
+        setNomPrix(event.target.value)
     }
+
+    let nomPrix = prixData.map((_) => _.nom_label).map((_) => (_ ? _.toLowerCase() : ''))
+    let o = {}
+    for (let s of nomPrix) o[s] = null
+    nomPrix = Object.keys(o)
+        .filter((s) => s.length > 0)
+        .sort()
+
+        useEffect(() => {
+            fetchData()
+          }, [])
+
+    return prixData.length === 0 ? (
+        <Container maxWidth='md' align='center'>
+            <CircularProgress />
+        </Container>
+    ) : (
+        <>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id='type-select-label'>Noms de Prix</InputLabel>
+                    <Select
+                        labelId='nom-select-label'
+                        id='nom-select'
+                        onChange={handleNomPrixChange}
+                        value={nameP}
+                    >
+                        <MenuItem value=''>
+                            <em>Pas de filtre</em>
+                        </MenuItem>
+                        {nomPrix.map((s) => (
+                            <MenuItem key={s} value={s}>
+                                {s}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <div style={{ maxWidth: '100%' }}>
+                    <MaterialTable
+                        title='Liste des prix recensés'
+                        columns={[
+                            { title: 'Nom du prix', field: 'nom_label', defaultFilter: nameP},
+                            { title: 'Discipline', field: 'discipline_label' },
+                            { title: "Année d'attribution", field: 'année' },
+                            { title: 'Prénom lauréat.e', field: 'élève_prénom' },
+                            { title: 'Nom lauréat.e', field: 'élève_nom' },
+                            { title: 'Cote AN du registre lauréat.e', field: 'élève_cote_AN_registre' },
+                            { title: 'Sexe lauréat.e', field: 'élève_sexe', sorting: false }
+                        ]}
+                        options={{
+                            pageSize: 50,
+                            pageSizeOptions: [20, 50, 100],
+                            filtering: true,
+                            sorting: true,
+                            cellStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' },
+                            headerStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' }
+                        }}
+                        data={prixData}
+                        onRowClick={((evt, selectedRow) => {
+                            const eleveId = selectedRow.élève.slice(-36)
+                            history.push('/eleve/' + eleveId)
+                        })}
+                    >
+                    </MaterialTable>
+                </div>
+            </>
+    )
 }
 
-export default withRouter(prix)
+export default withRouter(Prix)
 
