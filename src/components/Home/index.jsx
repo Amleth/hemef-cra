406a31da68b97fbf3af3f917305c9c4bfd37bab0
Thomas <thomas.bottini@cnrs.fr>
2020-11-06 09:36:02 +0100

    Élève

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,23 +1,22 @@
-import { Box } from '@material-ui/core'
-import { Container } from '@material-ui/core'
-import { Grid } from '@material-ui/core'
-import { List } from '@material-ui/core'
-import { ListItem } from '@material-ui/core'
-import { ListItemText } from '@material-ui/core'
-import { ListItemAvatar } from '@material-ui/core'
-import { Avatar } from '@material-ui/core'
-import { Typography } from '@material-ui/core'
-import { Search as SearchIcon } from '@material-ui/icons'
-import { ViewList as ViewListIcon } from '@material-ui/icons'
-import { Group as GroupIcon } from '@material-ui/icons'
-import { makeStyles } from '@material-ui/core/styles'
-import { DeveloperMode as DeveloperModeIcon } from '@material-ui/icons'
-import React from 'react'
-import { withRouter } from 'react-router'
+import { Box } from '@material-ui/core';
+import { Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { List } from '@material-ui/core';
+import { ListItem } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
+import { ListItemAvatar } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons';
+import { Group as GroupIcon } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import { DeveloperMode as DeveloperModeIcon } from '@material-ui/icons';
+import React from 'react';
+import { withRouter } from 'react-router';
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   list: {
     margin: 0,
@@ -25,13 +24,13 @@ const useStyles = makeStyles((theme) => ({
     '& li': {
       margin: 0,
       marginBottom: theme.spacing(2),
-      padding: 0
-    }
-  }
-}))
+      padding: 0,
+    },
+  },
+}));
 
 function Home() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Container maxWidth='md'>
       <Box pt={5} pb={10}>
@@ -42,11 +41,7 @@ function Home() {
       <Grid container className={classes.gridContainer} spacing={10}>
         <Grid item xs={7}>
           <Typography variant='body1' color='textSecondary' align='justify'>
-            Cette base de données recense les élèves, musiciens ou comédiens, inscrits au
-            Conservatoire national de Paris depuis sa fondation en 1795 jusqu’en 1914. Elle permet
-            de suivre leur cursus au sein du Conservatoire et de croiser ces données avec celles qui
-            concernent leur état civil, origine sociale, origine géographique, cursus antérieur et
-            postérieur à celui du Conservatoire.
+            Cette base de données recense les élèves, musiciens ou comédiens, inscrits au Conservatoire national de Paris depuis sa fondation en 1795 jusqu’en 1914. Elle permet de suivre leur cursus au sein du Conservatoire et de croiser ces données avec celles qui concernent leur état civil, origine sociale, origine géographique, cursus antérieur et postérieur à celui du Conservatoire.
           </Typography>
         </Grid>
         <Grid item xs={5}>
@@ -57,10 +52,7 @@ function Home() {
                   <SearchIcon />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText
-                primary='Responsables scientifiques'
-                secondary='Marie DUCHÊNE-THÉGARID & Cécile REYNAUD'
-              />
+              <ListItemText primary='Responsables scientifiques' secondary='Marie DUCHÊNE-THÉGARID & Cécile REYNAUD' />
             </ListItem>
 
             <ListItem disableGutters={true}>
@@ -69,10 +61,7 @@ function Home() {
                   <DeveloperModeIcon />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText
-                primary='Modélisation sémantique & développement logiciel'
-                secondary='Pierre LA ROCCA & Thomas BOTTINI'
-              />
+              <ListItemText primary='Modélisation sémantique & développement logiciel' secondary='Pierre LA ROCCA & Thomas BOTTINI' />
             </ListItem>
 
             <ListItem disableGutters={true}>
@@ -85,7 +74,7 @@ function Home() {
                 primary='Contributeur•rice•s'
                 secondary='Catherine MEROT,
 Valérie BARBAT, 
-Gabriela Elgarrista, 
+Gabriela ELGARRISTA, 
 Emmanuel HERVE, 
 Yvette ISSELIN,
 Françoise LAURENDEAU,
@@ -97,7 +86,7 @@ Anastasiia SYREISCHIKOVA'
         </Grid>
       </Grid>
     </Container>
-  )
+  );
 }
 
-export default withRouter(Home)
+export default withRouter(Home);
--- src/components/eleve/index.jsx
+++ src/components/eleve/index.jsx
@@ -1,145 +1,116 @@
-import React from 'react'
-import { withRouter } from 'react-router'
-import axios from 'axios'
-import {
-  CircularProgress,
-  Grid,
-  TextField,
-  Container,
-  Typography
-} from '@material-ui/core'
-import MaterialTable from 'material-table'
+import dayjs from 'dayjs';
+import { CircularProgress, TextField, Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialTable from 'material-table';
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router';
 
-class eleve extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      eleveData: null,
+const useStyles = makeStyles((theme) => ({
+  // root: {
+  //   alignItems: 'flex-start',
+  //   display: 'flex',
+  // },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 0,
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      marginLeft: 0,
+      marginRight: theme.spacing(4),
+    },
+  },
+}));
+
+function Eleve({ history, match }) {
+  const classes = useStyles();
+  const id = match.params.id;
+
+  const [eleve, setEleve] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://data-iremus.huma-num.fr/api/hemef/eleve/' + id);
+      res.json().then((res) => {
+        res.parcours = res.parcours.map((_) => ({
+          ..._,
+          date_entrée: formatDate(_['date_entrée'], _['hypothèse_date_entrée']),
+          date_sortie: formatDate(_['date_sortie'], _['hypothèse_date_sortie']),
+        }));
+        setEleve({
+          ...res,
+          date_de_naissance: formatDate(res['date_de_naissance'], res['hypothèse_date_de_naissance']),
+          cursus_date_entree_conservatoire: formatDate(res['cursus_date_entree_conservatoire'], res['hypothese_cursus_date_entree_conservatoire']),
+          cursus_date_sortie_conservatoire: formatDate(res['cursus_date_sortie_conservatoire'], res['hypothese_cursus_date_sortie_conservatoire']),
+        });
+      });
     }
-  }
-  //a adapter
-  componentDidMount() {
-    const id = this.props.match.params.id
+    fetchData();
+  }, [id]);
 
-    axios.get('http://data-iremus.huma-num.fr/api/hemef/eleve/' + id).then(res => {
-
-      this.setState({ eleveData: res.data })
-    }
-    )
-  }
-
-  render() {
-    if (!this.state.eleveData) {
-      return (<Container maxWidth='md' align='center'>
-      <CircularProgress />
-    </Container>)
-    } else {
-      //affichage conditionnel pour le champ date de sortie
-      let eleve = this.state.eleveData
-      let intitule_date_sortie_conservatoire = null
-      let champ_date_sortie_conservatoire = null
-      if (this.state.eleveData.hypothese_cursus_date_sortie_conservatoire) {
-        intitule_date_sortie_conservatoire = "Date de sortie supposée :"
-        champ_date_sortie_conservatoire = this.state.eleveData.hypothese_cursus_date_sortie_conservatoire
-      }
-      else if (this.state.eleveData.cursus_date_sortie_conservatoire) {
-        intitule_date_sortie_conservatoire = "Date de sortie :"
-        champ_date_sortie_conservatoire = makeDate(this.state.eleveData.cursus_date_sortie_conservatoire)
-      }
-
-      //affichage conditionnel pour le champ motif de sortie
-      let intitule_motif_sortie_conservatoire = null
-      let champ_motif_sortie_conservatoire = null
-      if (this.state.eleveData.hypothese_cursus_motif_sortie) {
-        intitule_motif_sortie_conservatoire = "Motif de sortie supposé :"
-        champ_motif_sortie_conservatoire = this.state.eleveData.hypothese_cursus_motif_sortie
-      }
-      else if (this.state.eleveData.cursus_motif_sortie) {
-        intitule_motif_sortie_conservatoire = "Motif de sortie :"
-        champ_motif_sortie_conservatoire = this.state.eleveData.cursus_motif_sortie
-      }
-
-
-      //affichage conditionnel du pseudonyme
-      let infos_eleve = null
-
-      const dateNaissance = 
-        eleve.date_de_naissance ? makeDate(eleve.date_de_naissance) :
-        eleve.hypothèse_date_de_naissance ? eleve.hypothèse_date_de_naissance + ' (hypothèse)' : null 
-    
-
-      if (this.state.eleveData.pseudonyme) {
-        infos_eleve =
-          <Grid container direction='column' >
-            {makeTextField('Nom', eleve.nom)}
-            {makeTextField('Prénom', eleve.prenom)}
-            {makeTextField('Sexe', eleve.sexe)}
-            {makeTextField('Date de naissance', dateNaissance)}
-            {makeTextField('Lieu de naissance', eleve.nait_a_label)}
-            {makeTextField('Pseudonyme', eleve.pseudonyme)}
-          </Grid>
-      } else {
-        infos_eleve =
-          <Grid container direction='column' >
-            {makeTextField('Nom', eleve.nom)}
-            {makeTextField('Prénom', eleve.prenom)}
-            {makeTextField('Sexe', eleve.sexe)}
-            {makeTextField('Date de naissance', dateNaissance)}
-            {makeTextField('Lieu de naissance', eleve.nait_a_label)}
-          </Grid>
-      }
-
-      //Creation conditionnelle des infos_cursus
-      let infos_cursus =
-        <Grid container direction='column' >
+  if (Object.entries(eleve).length === 0) {
+    return (
+      <Container maxWidth='md' align='center'>
+        <CircularProgress />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <Typography component='h1' variant='h3'>
+          Élève
+        </Typography>
+        <div className={classes.form}>
+          {makeTextField('Nom', eleve.nom)}
+          {makeTextField('Prénom', eleve.prenom)}
+          {makeTextField('Sexe', eleve.sexe)}
+          {makeTextField('Date de naissance', eleve.date_de_naissance)}
+          {makeTextField('Lieu de naissance', eleve.nait_a_label)}
+          {eleve.pseudonyme && makeTextField('Pseudonyme', eleve.pseudonyme)}
+        </div>
+        <br />
+        <br />
+        <Typography component='h2' variant='h5'>
+          Informations de cursus
+        </Typography>
+        <div className={classes.form}>
           {makeTextField('Cote AN Registre', eleve.cote_AN_registre)}
-          <br />
-          {makeTextField("Date d'entrée au concervatoire ", makeDate(eleve.cursus_date_entree_conservatoire))}
+          {makeTextField("Date d'entrée au conservatoire ", eleve.cursus_date_entree_conservatoire)}
           {makeTextField("Motif d'entrée", eleve.cursus_motif_admission)}
-          <br />
-          {makeTextField(intitule_date_sortie_conservatoire, champ_date_sortie_conservatoire)}
-          {makeTextField(intitule_motif_sortie_conservatoire, champ_motif_sortie_conservatoire)}
-        </Grid>
-
-      return (
-        <Container>
-          <Typography component='h1' variant='h3'>Page de l'élève</Typography>
-          {infos_eleve}
-          <br />
-          <br />
-          <Typography component='h2' variant='h5'>Informations de cursus</Typography>
-          {infos_cursus}
-          <br />
-          <br />
-          <MaterialTable
-            title="Classes suivies"
-            columns={[
-              { title: "Discipline", field: "discipline_enseignée_label" },
-              { title: "Professeur", field: "professeur_label" }, {
-                title: "Date d'entrée", render: row => {
-                  let date = row.date_entrée.split('T')[0]
-                  date = date.split('-')
-                  return (date[2] + "/" + date[1] + "/" + date[0])
-                }
-              }
-            ]}
-            options={{
-              pageSize: 5,
-              pageSizeOptions: [5, 10, 20],
-              cellStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' },
-              headerStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' }
-            }}
-            data={this.state.eleveData.parcours}
-            onRowClick={(evt, selectedRow) => {
-              const parcoursClasseID = selectedRow.parcours.slice(-36)
-              this.props.history.push('/classe_cursus/' + parcoursClasseID)
-            }}
-
-          />
-        </Container>
-
-      )
-    }
+          {makeTextField('Date de sortie du conservatoire', eleve.cursus_date_sortie_conservatoire)}
+          {makeTextField('Motif de sortie du conservatoire', eleve.cursus_motif_sortie)}
+        </div>
+        <br />
+        <br />
+        <MaterialTable
+          title='Classes suivies'
+          columns={[
+            { title: 'Discipline', field: 'discipline_enseignée_label' },
+            { title: 'Professeur', field: 'professeur_label' },
+            {
+              title: "Date d'entrée",
+              field: 'date_entrée',
+            },
+            {
+              title: 'Date de sortie',
+              field: 'date_sortie',
+            },
+          ]}
+          options={{
+            pageSize: 5,
+            pageSizeOptions: [5, 10, 20],
+            cellStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' },
+            headerStyle: { paddingBottom: '0.3em', paddingTop: '0.3em' },
+          }}
+          data={eleve.parcours}
+          onRowClick={(evt, selectedRow) => {
+            const parcoursClasseID = selectedRow.parcours.slice(-36);
+            history.push('/classe_cursus/' + parcoursClasseID);
+          }}
+        />
+      </Container>
+    );
   }
 }
 
@@ -148,20 +119,29 @@ function makeTextField(f, v) {
     <TextField
       label={f}
       defaultValue={v}
+      fullWidth={false}
       InputProps={{
-        readOnly: true
+        readOnly: true,
       }}
     />
-  )
+  );
 }
 
-function makeDate(d) {
-  if (d) {
-    let tempDate = d.split('^^')[0]
-    return (tempDate.split('-')[2] + '/' + tempDate.split('-')[1] + '/' + tempDate.split('-')[0])
-  }
+// hemef:date_de_naissance
+// hemef:hypothèse_date_de_naissance
+// hemef:cursus_date_entree_conservatoire
+// hemef:hypothese_cursus_date_entree_conservatoire
+// hemef:cursus_date_sortie_conservatoire
+// hemef:hypothese_cursus_date_sortie_conservatoire
+// parcours — hemef:date_entree
+// parcours — hemef:hypothèse_date_entrée
+// parcours — hemef:date_sortie
+// parcours — hemef:hypothèse_date_sortie
+function formatDate(d, h) {
+  d = d ? d.split('^^')[0] : '';
+  h = h ? h.replace('[', '').replace(']', '') + ' (hypothèse)' : '';
+  d = d ? dayjs(d).format('DD/MM/YYYY') : '';
+  return d || h;
 }
 
-export default withRouter(eleve)
-
-
+export default withRouter(Eleve);
